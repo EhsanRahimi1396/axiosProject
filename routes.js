a99266e45411783b9bf6e30adec5032ab25c1a78
EhsanRahimi1396 <72453993+EhsanRahimi1396@users.noreply.github.com>
2020-10-14 01:32:07 -0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,45 +2,14 @@ const express = require('express');
 const router = express.Router();
 const controller=require('./controller');
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: ' GET REQUEST'
-    })
+router
+    .route('/')
+    .get(controller.getinformation)
+    .post(controller.postinformation)
+    .put(controller.putinformation)
+    .delete(controller.deleteinformation)
 
-}).catch(err => {
-    res.status(500).json({
-        error: err
-    })
-})
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: ' POST REQUEST'
-    })
-}).catch(err => {
-    res.status(500).json({
-        error: err
-    })
-})
-
-router.patch('/', (req, res, next) => {
-    res.status(200).json({
-        message: ' PATCH REQUEST'
-    })
-}).catch(err =>{
-    res.status(500).json({
-        error:err
-    })
-})
-
-router.delete('/', (req, res, next) => {
-    res.status(200).json({
-        message:'DELETE REQUEST'
-    })
-}).catch(err =>{
-    res.status(500).json({
-        error:err
-    })
-})
-
+       
+  
 module.exports = router;
