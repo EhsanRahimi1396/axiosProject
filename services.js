d4063fbcc0c0194cdf4f83b3eee531b47a22db77
EhsanRahimi1396 <72453993+EhsanRahimi1396@users.noreply.github.com>
2020-10-18 06:10:16 -0700

    Update services.js

--- services.js
+++ services.js
@@ -1,119 +1,60 @@
 const express = require('express');
 const axios = require('axios');
+async function getTodos() {
 
+    try {
+        const response = await axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
+        console.log(response);
+        return response;
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+module.exports = {
+    getTodos
+}
 
-axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
-console.log(res)
-    //.then(result => {
-    // res.status(200).json({
-    //    message: 'you sent get request'
-    // })
-    //}).catch(err => {
-    //     console.log(err);
-    //  res.status(500).json({
-    // error: err
-    //})
+async function postTodos() {
+    try {
+        const response = await axios.post('http://jsonplaceholder.typicode.com/todos?_limit=5')
+        console.log(response);
+        return response;
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+module.exports = {
+    postTodos
+}
 
-    // })
-
-    .then((res) => {
-        //console.log(res.status.code);
-        console.log(res.data)
-        console.log(res.status);
-        response.json({
-            message: "you send Get request"
-            
-        })
-    return data ? data.result : null
-    })
-
-    .catch((error) => {
-        console.error(error)
-        response.json(" you accepted error")
-        return error ? error.result :null
-    })
-
-
-
-axios.post('http://jsonplaceholder.typicode.com/todos?_limit=5')
-console.log(res)
-    //.then(result => {
-    //  res.status(200).json({
-    // message: 'you sent post request'
-    //  })
-    //}).catch(err => {
-    //  res.status(500).json({
-    // error: err
-    //  })
-    // })
-    .then((res) => {
-        console.log(res.data)
-        console.log(res.status);
-        response.json({
-            message: ' you send Post request'
-        })
-        return data ? data.result: null
-    })
-    .catch((error) => {
-        console.log(error)
-        response.json({
-            message: 'you accepted error'
-        })
-        return error ? error.result :null
-    })
-
-axios.delete('http://jsonplaceholder.typicode.com/todos?_limit=5')
-//.then(result => {
-// res.status(200).json({
-//  message: 'you sent delete request'
-
-
-// })
-// }).catch(err => {
-//  res.status(500).json({
-// error: err
-// })
-// })
-console.log(res)
-
-    .then((res) => {
-        console.log(res.data)
-        console.log(res.status);
-        response.json({
-            message: 'you send delete request'
-        })
-        return data ?data.result :null
-    })
-    .catch((error) => {
-        console.log(error)
-        response.json('you accepted error')
-        return error ? error.result :null
-    })
-
-axios.put('http://jsonplaceholder.typicode.com/todos?_limit=5')
-// .then(result => {
-// res.status(200).json({
-// message: 'you sent put request'
-// })
-// }).catch(err => {
-//  res.status(500).json({
-// error: err
-// })
-//  })
-console.log(res)
-
-    .then((res) => {
-        console.log(res.data)
-        console.log(res.status);
-        response.json({
-            message: 'you send put request'
-        })
-        return data ? data.result :null
-    })
-    .catch((error) => {
-        console.log(error)
-        response.json('you accepted error')
-            return error ? error.result :null
-    })
+async function putTodos() {
+    try {
+        const response = await axios.put('http://jsonplaceholder.typicode.com/todos?_limit=5')
+        console.log(response);
+        return response;
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+module.exports = {
+    putTodos
+}
+ 
+async function deleteTodos(){
+    try {
+        const response =await axios.delete('http://jsonplaceholder.typicode.com/todos?_limit=5')
+        console.log(response);
+        return response;
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+module.exports={
+    deleteTodos
+}
 
 
