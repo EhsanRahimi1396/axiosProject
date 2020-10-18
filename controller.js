d2403eb7f3e282016cc90afd08e890dab93187bf
EhsanRahimi1396 <72453993+EhsanRahimi1396@users.noreply.github.com>
2020-10-18 06:02:58 -0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,62 +1,62 @@
 const Services = require('./services')
-
 module.exports.getinformation = (req, res, next) => {
-     res.send().json({
-        respons: {
-            status: res.status,
-            data: res.data,
-        }
-    })
-        .catch(err => {
-            res.status(500).json({
-                error: err,
-            })
+    try {
+        res.send().json({
+            respons: {
+                status: res.status,
+                data: res.data,
+            }
         })
-      
- 
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
+
+
 module.exports.postinformation = (req, res, next) => {
-    res.send().json({
-        respons: {
-            status: res.status,
-            data: res.data,
-        }
-    })
-        .catch(err => {
-            res.status(500).json({
-                error: err,
-            })
+    try {
+        res.send().json({
+            respons: {
+                status: res.status,
+                data: res.data
+            }
         })
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 module.exports.deleteinformation = (req, res, next) => {
-     res.send().json({
-        respons: {
-            status: res.status,
-            data: res.data,
-        }
-    })
-        .catch(err => {
-            res.status(500).json({
-                error: err,
-            })
+    try {
+        res.send().json({
+            respons: {
+                status: res.status,
+                data: res.data
+            }
         })
-
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 module.exports.putinformation = (req, res, next) => {
-     res.send().json({
-        respons: {
-            status: res.status,
-            data: res.data,
-        }
-    })
-        .catch(err => {
-            res.status(500).json({
-                error: err,
-            })
+    try {
+        res.send().json({
+            respons: {
+                status: res.status,
+                data: res.data
+            }
         })
+
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
+
 
 
